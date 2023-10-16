2496b4fc9f4fc5e33bead9c4865c7813d041f229
Enzo Rossetto <enzo.rossetto@ifood.com.br>
2023-10-16 16:52:27 -0300

    feat: create CardView and apply isFaceUp logic

---
+++ Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Enzo Rossetto on 12/10/23.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
